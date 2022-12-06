6e57a54c2d1dfa5e9f844d8e1f25d43c6a6e59a3
Alan Cleary <acleary@ncgr.org>
2022-12-06 16:04:22 -0700

    Added a submit method to the form wrapper component.
    Calling the method will programmatically submit the form wrapped by the component.

--- src/core/lis-form-wrapper-element.ts
+++ src/core/lis-form-wrapper-element.ts
@@ -1,9 +1,32 @@
 import {LitElement, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, queryAssignedElements} from 'lit/decorators.js';
 
 
 @customElement('lis-form-wrapper-element')
 export class LisFormWrapperElement extends LitElement {
+
+  // bind to the forms in the slot
+  @queryAssignedElements({selector: 'form'})
+  private _forms!: HTMLFormElement[];
+
+  // allows the wrapped form to be submitted programmatically
+  submit() {
+    // throw an error if there's no form to submit
+    if (!this._forms.length) {
+      throw new Error('No form to submit');
+    }
+    // only submit the first form
+    const formElement = this._forms[0];
+    // get the form's submit element
+    const submitElement: HTMLElement | null = formElement.querySelector('[type="submit"]');
+    // use the element to submit the form if it exists
+    if (submitElement !== null) {
+      formElement.requestSubmit(submitElement);
+    // otherwise, use the form as the submit element
+    } else {
+      formElement.requestSubmit();
+    }
+  }
 
   // called when the form in the template slot is submitted
   private _submit(e: Event) {
