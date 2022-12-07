4c4eb3882db35b2678c7d26ac0ba27addd2ef33b
Alan Cleary <acleary@ncgr.org>
2022-12-07 12:38:06 -0700

    Enhanced query string parameter controller functionality.
    The controller now only updates the query string if at least one of the parameters given has a different value than what's currently in the URL query string. The controller now also triggers a redraw in the host when the URL query string changes. All popstate listeners are delayed until the redraw is complete in case a listener depends on its template being updated first.

--- src/controllers/lis-query-string-parameters-controller.ts
+++ src/controllers/lis-query-string-parameters-controller.ts
@@ -19,6 +19,7 @@ export class LisQueryStringParametersController implements ReactiveController {
     window.removeEventListener('popstate', this._popState.bind(this));
   }
 
+  // returns the current value of a URL parameter
   getParameter(name: string, defaultValue: string = ''): string {
     const params = new URLSearchParams(window.location.search);
     const value: string | null = params.get(name);
@@ -28,7 +29,13 @@ export class LisQueryStringParametersController implements ReactiveController {
     return defaultValue;
   }
 
+  // updates the URL parameters
   setParameters(parameters: Object): void {
+    // don't update the query string if there's nothing to update
+    if (!this._differentValues(parameters)) {
+      return;
+    }
+    // push the new params onto the browser's history stack
     const queryString = '?' +
       Object.entries(parameters)
         .map(([key, value]) => `${key}=${value}`)
@@ -36,14 +43,31 @@ export class LisQueryStringParametersController implements ReactiveController {
     history.pushState(parameters, '', queryString);
   }
 
+  // adds a listener to the popstate event
   addListener(listener: EventListener): void {
+    // each listener is called in the scope of the host
     this._listeners.push(listener.bind(this.host));
   }
 
+  // determines if any of the given parameters have different values than the URL parameters
+  private _differentValues(parameters: Object): boolean {
+    const params = new URLSearchParams(window.location.search);
+    return Object.entries(parameters)
+      .some(([key, value]) => value.toString() !== params.get(key));
+  }
+
+  // calls all listeners of the popstate event
   private _popState(event: PopStateEvent): void {
-    this._listeners.forEach((listener) => {
-      listener(event);
-    });
+    // redraw the host
+    this.host.requestUpdate();
+    // wait for the redraw to complete in case any listeners rely on state from the template
+    this.host.updateComplete
+      .then(() => {
+        // call each listener
+        this._listeners.forEach((listener) => {
+          listener(event);
+        });
+      });
   }
 
 }
