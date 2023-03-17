a35debf349eb3346ce77a4cf412b62c5e6ee0d17
Alan Cleary <acleary@ncgr.org>
2023-03-17 13:27:22 -0600

    Implemented slot controller
    The slot controller allows Web Components that disable the Shadow DOM to still use slots.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -9,3 +9,4 @@
 export * from './lis-cancel-promise-controller';
 export * from './lis-dom-content-loaded-controller';
 export * from './lis-query-string-parameters-controller';
+export * from './lis-slot-controller';
