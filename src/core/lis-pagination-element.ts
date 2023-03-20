9fb36169d37fc46daa6660c0f33b5e5781f56597
Alan Cleary <acleary@ncgr.org>
2023-03-20 12:05:29 -0600

    Fixed a bug in the pagination element.
    Specifically, the next page could be loaded programmatically when the hasNext attribute/property was false. This has been corrected so now the page can't be changed via the UI or programmatically unless the hasNext attribute/property is true.

--- src/core/lis-pagination-element.ts
+++ src/core/lis-pagination-element.ts
@@ -137,8 +137,10 @@ export class LisPaginationElement extends LitElement {
     if (e !== undefined) {
       e.preventDefault();
     }
-    this.page += 1;
-    this._dispatchPageChange();
+    if (this.hasNext) {
+      this.page += 1;
+      this._dispatchPageChange();
+    }
   }
 
   /** @ignore */
