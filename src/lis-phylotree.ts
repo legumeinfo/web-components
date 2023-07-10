31eae13e33cc4d225c0ecb7872eec827b02edb75
Rishab Pradeep <rishab.a.pradeep@gmail.com>
2023-07-10 16:10:20 -0500

    Updated  web-component with LisPhylotree class

---
+++ src/lis-phylotree.ts
@@ -0,0 +1,14 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('lis-phylotree')
+export class LisPhylotree {
+    render() {
+        return html`
+        <link rel="stylesheet" href="http://tntvis.github.io/tnt.tree/build/tnt.tree.css" type="text/css" />
+        <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+        <script src="http://tntvis.github.io/tnt.tree/build/tnt.tree.min.js" charset="utf-8"></script>
+        `;
+    }
+}
+
