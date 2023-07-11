70a4d507485885365ee0b819b398ac8dc3d7b4bb
Rishab <42660337+rispop@users.noreply.github.com>
2023-07-11 13:40:35 -0500

    Update lis-phylotree.ts

--- src/lis-phylotree.ts
+++ src/lis-phylotree.ts
@@ -4,15 +4,18 @@ import {customElement, property} from 'lit/decorators.js';
 @customElement('lis-phylotree')
 export class LisPhylotree extends LitElement {
     @property()
+    input: string = "";
+
+
     
     override render() {
-        return html`
-        <link rel="stylesheet" href="http://tntvis.github.io/tnt.tree/build/tnt.tree.css" type="text/css" />
-        <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
-        <script src="http://tntvis.github.io/tnt.tree/build/tnt.tree.min.js" charset="utf-8"></script>
-        
-        <!-- example script from TnT -->
-        <script>
+        this.makeTree()
+        return html``
+                
+    ;
+    }
+    makeTree()
+    {
         var tree = tnt.tree()
             .data(tnt.tree.parse_newick("((human, chimp),mouse)"))
             .width(500)
@@ -51,9 +54,6 @@ export class LisPhylotree extends LitElement {
             .tree(tree)
             .board(board)
             .track(track);
-            vis(document.getElementById("mydiv"));
-    </script>
-    `;
+            vis(this);
     }
-
 }
