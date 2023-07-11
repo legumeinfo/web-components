6e209a425989dcb3fe158f866d8cf46137c7a8bb
Rishab <42660337+rispop@users.noreply.github.com>
2023-07-11 10:49:35 -0500

    Update lis-phylotree.ts

--- src/lis-phylotree.ts
+++ src/lis-phylotree.ts
@@ -10,6 +10,50 @@ export class LisPhylotree extends LitElement {
         <link rel="stylesheet" href="http://tntvis.github.io/tnt.tree/build/tnt.tree.css" type="text/css" />
         <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
         <script src="http://tntvis.github.io/tnt.tree/build/tnt.tree.min.js" charset="utf-8"></script>
-        `;
+        
+        <!-- example script from TnT -->
+        <script>
+        var tree = tnt.tree()
+            .data(tnt.tree.parse_newick("((human, chimp),mouse)"))
+            .width(500)
+
+        // The board
+        var board = tnt.board()
+            .width(400)
+            .from(0)
+            .to(1000)
+            .max(1000);
+
+        // The function to create tracks for each tree leaf
+        var track = function (leaf) {
+            var data = leaf.data();
+
+            return tnt.board.track()
+                .color("white")
+                .data(tnt.board.track.data.sync()
+                    .retriever (function () {
+                        var elems = [];
+                        // populate elems and then return them
+                        return elems;
+                    })
+                )
+                .display(tnt.board.track.feature.block()
+                    .color("steelblue")
+                    .index(function (d) {
+                        return d.start;
+                    })
+                );
+        }
+
+
+        // The TnT Vis
+        var vis = tnt()
+            .tree(tree)
+            .board(board)
+            .track(track);
+            vis(document.getElementById("mydiv"));
+    </script>
+    `;
     }
+
 }
