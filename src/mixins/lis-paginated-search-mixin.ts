e6607a570b35bb9553a561842d61fa935a604b5f
Alan Cleary <acleary@ncgr.org>
2023-08-03 21:45:10 -0600

    The paginated search table is now configurable at run-time.
    Specifically, the resultAttribute and tableHeader class attributes were converted from protected states to public properties so they can be set at run-time via JavaScript.

--- src/mixins/lis-paginated-search-mixin.ts
+++ src/mixins/lis-paginated-search-mixin.ts
@@ -102,6 +102,29 @@ export declare class LisPaginatedSearchElementInterface<SearchData, SearchResult
 
   /**
    * Components that use the
+   * {@link LisPaginatedSearchMixin | `LisPaginatedSearchMixin`} mixin must
+   * define what attributes their search results will have so the mixin can
+   * correctly parse and display the results in a table. These attributes
+   * can be specified by setting this property in a component's constructor.
+   * Additionally, this property may be used by the end user at run-time to override the
+   * default result attributes defined by the component.
+   */
+  resultAttributes: string[];
+
+  /**
+   * Components that use the
+   * {@link LisPaginatedSearchMixin | `LisPaginatedSearchMixin`} mixin must
+   * define what attributes their search results will have so the mixin can
+   * correctly parse and display the results in a table. The header of the
+   * table is set from an object that has these attributes. The object can
+   * be specified by setting this property in a component's constructor.
+   * Additionally, this property may be used by the end used at run-time to override the
+   * default table headers defined by the component.
+   */
+  tableHeader: Object;
+
+  /**
+   * Components that use the
    * {@link LisPaginatedSearchMixin | `LisPaginatedSearchMixin`} mixin will
    * inherit this method. It allows the component's search form to be submitted
    * programmatically.
@@ -154,28 +177,9 @@ export declare class LisPaginatedSearchElementInterface<SearchData, SearchResult
   protected requiredQueryStringParams: string[][];
 
   /**
-   * Components that use the
-   * {@link LisPaginatedSearchMixin | `LisPaginatedSearchMixin`} mixin must
-   * define what attributes their search results will have so the mixin can
-   * correctly parse and display the results in a table. These attributes
-   * can be specified by setting this property in a component's constructor.
-   */
-  public resultAttributes: string[];
-
-  /**
    * The results returned by the `searchFunction`.
    */
   public searchResults: SearchResult[];
-
-  /**
-   * Components that use the
-   * {@link LisPaginatedSearchMixin | `LisPaginatedSearchMixin`} mixin must
-   * define what attributes their search results will have so the mixin can
-   * correctly parse and display the results in a table. The header of the
-   * table is set from an object that has these attributes. The object can
-   * be specified by setting this property in a component's constructor.
-   */
-  public tableHeader: Object;
 
   /**
    * When the form of a component that use the
@@ -413,13 +417,17 @@ class LisPaginatedSearchElement extends superClass {
   searchFunction: SearchFunction<SearchData, SearchResult> =
     () => Promise.reject(new Error('No search function provided'));
 
+  // attributes of result objects in the concrete class
+  @property()
+  resultAttributes: string[] = [];
+
+  // the table headers to use in the concrete class
+  @state()
+  tableHeader: Object = {};
+
   // what form parts are required to submit a search
   @state()
   protected requiredQueryStringParams: string[][] = [];
-
-  // attributes of result objects in the concrete class
-  @state()
-  protected resultAttributes: string[] = [];
 
   // keep a copy of the search results for template generalization
   @state()
@@ -428,10 +436,6 @@ class LisPaginatedSearchElement extends superClass {
   // info about the search results
   @state()
   protected resultsInfo: string = '';
-
-  // the tables headers to use in the concrete class
-  @state()
-  protected tableHeader: Object = {};
 
   // keep a copy of the search form data for pagination
   @state()
