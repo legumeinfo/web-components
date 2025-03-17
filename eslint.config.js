df884a370af57ecf87c063f39d1275ea5f5c6e73
Alan Cleary <acleary@ncgr.org>
2025-03-17 13:08:21 -0600

    Disabled no-explicit-any ESlint rule

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default [
       sourceType: "module",
     },
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unused-vars": ["warn", {
         "argsIgnorePattern": "^_",
